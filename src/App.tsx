cdeba4cc3bff4d87a4af82baca7d013220bd3d57
Ifycode <marychord@gmail.com>
2022-03-05 00:37:33 +0100

    Update: Default props

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,26 @@ function HeadingWithChildrenElement({ children }: { children: ReactNode }): Reac
   );
 }
 
+// Default props
+// (The heading prop has a default in it - don't understand this though)
+
+const defaultContainerProps = {
+  heading: <strong> My heading</strong>
+}
+
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps;
+
+function Container({ heading, children }: ContainerProps): ReactElement {
+  return (
+    <div>
+      <h1>{heading}</h1>
+      {children}
+    </div>
+  )
+}
+
+Container.defaultProps = defaultContainerProps;
+
 function App() {
   return (
     <div className="App">
@@ -30,8 +50,25 @@ function App() {
       <HeadingWithChildrenElement>
         <strong>Strong children header</strong>
       </HeadingWithChildrenElement>
+
+      <Container>Foo</Container>
     </div>
   );
 }
 
 export default App;
+
+
+// default props (previous)
+
+// const defaultContainerProps = {
+//   heading: <strong> My heading</strong>
+// }
+// function Container({ heading, children }: { children: ReactNode } & typeof defaultContainerProps): ReactElement {
+//   return (
+//     <div>
+//       <h1>{heading}</h1>
+//       {children}
+//     </div>
+//   )
+// }
