b12866048367b3dbd2d45f6ee460c45d9f88541c
Ifycode <marychord@gmail.com>
2022-03-04 21:40:02 +0100

    Update: conventional props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
+
+// Conventional props
+function Heading({ title }: { title: string }) {
+  return (
+    <h1>{title}</h1>
+  );
+}
+
+function HeadingWithChildrenString({ children }: { children: string }) {
+  return (
+    <h1>{children}</h1>
+  );
+}
+
+function HeadingWithChildrenElement({ children }: { children: ReactNode }): ReactElement {
+  return (
+    <h1>{children}</h1>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Heading title="Sweet TS heading!"></Heading>
+      <HeadingWithChildrenString>
+        Children Header
+      </HeadingWithChildrenString>
+      <HeadingWithChildrenElement>
+        <strong>Strong children header</strong>
+      </HeadingWithChildrenElement>
     </div>
   );
 }
